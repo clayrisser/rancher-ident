0bc031bb945381065a78e30791435f32b104f78d
Jam Risser <jam@jamrizzi.com>
2017-01-13 05:21:45 -0600

    Added nfs mounting support

--- helper/Helper.py
+++ helper/Helper.py
@@ -9,9 +9,12 @@ class Helper:
             yum clean all
             rpm --rebuilddb
             yum update -y
+            yum install -y nfs-utils nfs-utils-lib
             ''')
         elif (platform.dist()[0] == 'Ubuntu'):
-            os.system('apt-get update -y')
+            os.system('''apt-get update -y
+            apt-get install -y nfs-common
+            ''')
         else:
             print('Operating system not supported')
             sys.exit('Exiting installer')
--- rancher-ident.py
+++ rancher-ident.py
@@ -12,7 +12,8 @@ def main():
     helper.is_root()
     options = gather_information(get_defaults())
     helper.prepare()
-    create_volumes_dir(options)
+    mount_volumes(options)
+    mount_backup_storage(options)
     install_docker()
     install_nginx(options)
     install_mariadb(options)
@@ -24,12 +25,12 @@ def get_defaults():
     return {
         'email': 'email@example.com',
         'rancher_domain': 'cloud.yourdomain.com',
+        'volumes_mount': 'local',
+        'backup_storage_mount': 'local',
         'backup_storage_target_url': '',
         'backup_storage_access_key': '',
         'backup_storage_secret_key': '',
-	    'backup_storage_volume': '/backup',
         'cron_schedule': '0 0 0 * * *',
-        'volumes_mount': 'local',
         'rancher_mysql_database': 'rancher',
         'mysql_root_password': 'hellodocker'
     }
@@ -38,12 +39,12 @@ def gather_information(defaults):
     options = {}
     options['email'] = default_prompt('Email', defaults['email'])
     options['rancher_domain'] = default_prompt('Rancher Domain', defaults['rancher_domain'])
-    options['backup_storage_volume'] = default_prompt('Backup Storage Volume', defaults['backup_storage_volume'])
+    options['volumes_mount'] = default_prompt('Volumes Mount', defaults['volumes_mount'])
+    options['backup_storage_mount'] = default_prompt('Backup Storage Mount', defaults['backup_storage_mount'])
     options['backup_storage_target_url'] = default_prompt('Backup Storage Target URL', defaults['backup_storage_target_url'])
     options['backup_storage_access_key'] = default_prompt('Backup Storage Access Key', defaults['backup_storage_access_key'])
     options['backup_storage_secret_key'] = default_prompt('Backup Storage Secret Key', defaults['backup_storage_secret_key'])
     options['cron_schedule'] = default_prompt('Cron Schedule', defaults['cron_schedule'])
-    options['volumes_mount'] = default_prompt('Volumes Mount', defaults['volumes_mount'])
     options['rancher_mysql_database'] = default_prompt('Rancher Mysql Database', defaults['rancher_mysql_database'])
     options['mysql_root_password'] = default_prompt('MYSQL Root Password', defaults['mysql_root_password'])
     return options
@@ -56,12 +57,22 @@ def default_prompt(name, fallback):
     else:
         return fallback
 
-def create_volumes_dir(options):
-    os.system('mkdir -p /volumes')
-    if os.system(options['volumes_mount']):
-        os.system('mkfs.xfs -i size=512 ' + options['volumes_mount'])
-        os.system('echo "' + options['volumes_mount'] + ' /volumes xfs defaults 1 2" >> /etc/fstab')
-        os.system('mount -a && mount')
+def mount_volumes(options):
+    mount(options['volumes_mount'], '/volumes')
+
+def mount_backup_storage(options):
+    mount(options['backup_storage_mount'], '/backup')
+
+def mount(mount_from, mount_to):
+    os.system('mkdir -p ' + mount_to)
+    if mount_from != 'local':
+        if mount_from[:4] == '/dev':
+            os.system('mkfs.xfs -i size=512 ' + mount_from)
+            os.system('echo "' + mount_from + ' ' +  mount_to + ' xfs defaults 1 2" | tee -a /etc/fstab')
+            os.system('mount -a && mount')
+        else:
+            os.system('mount -t nfs -o proto=tcp,port=2049 ' + mount_from + ' ' + mount_to)
+            os.system('echo "' + mount_from + ' ' + mount_to + ' nfs rsize=8192,wsize=8192,timeo=14,intr" | tee -a /etc/fstab')
 
 def install_docker():
 	os.system('''
@@ -118,7 +129,7 @@ def restore_volumes(options):
     os.system('''
     docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    ''' + (('-v ' + options['backup_storage_volume'] + ':/borg') if options['backup_storage_volume'] != '' else '') + ''' \
+    ''' + ('' if options['backup_storage_target_url'] == '' else '-v /backup:/borg') + ''' \
     -e STORAGE_ACCESS_KEY=''' + options['backup_storage_access_key'] + ''' \
     -e STORAGE_SECRET_KEY=''' + options['backup_storage_secret_key'] + ''' \
     -e STORAGE_TARGET_URL=''' + options['backup_storage_target_url'] + ''' \
@@ -130,7 +141,7 @@ def install_ident(options):
     os.system('''
     docker run -d --name ident --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    ''' + (('-v ' + options['backup_storage_volume'] + ':/borg') if options['backup_storage_volume'] != '' else '') + ''' \
+    ''' + ('' if options['backup_storage_target_url'] == '' else '-v /backup:/borg') + ''' \
     -e STORAGE_ACCESS_KEY=''' + options['backup_storage_access_key'] + ''' \
     -e STORAGE_SECRET_KEY=''' + options['backup_storage_secret_key'] + ''' \
     -e STORAGE_TARGET_URL=''' + options['backup_storage_target_url'] + ''' \
